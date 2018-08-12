1ad3ebde09c037d0093b8675bbfad712c5167c90
ranjeet <ranjeet.leecare@gmail.com>
2018-08-12 10:46:31 +1000

    Getting unauthorised error 401

--- companyendpoint/src/main/java/com/ranjeet/company/companyendpoint/model/Company.java
+++ companyendpoint/src/main/java/com/ranjeet/company/companyendpoint/model/Company.java
@@ -5,11 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import org.hibernate.annotations.Table;
+import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Entity
-@Table(appliesTo = "ltccompany")
+@Table(name = "ltccompany")
 public class Company {
 
   @Id
---
+++ companyendpoint/src/main/java/com/ranjeet/company/companyendpoint/restcontroller/CompanyRestController.java
@@ -0,0 +1,28 @@
+package com.ranjeet.company.companyendpoint.restcontroller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.ranjeet.company.companyendpoint.model.Company;
+import com.ranjeet.company.companyendpoint.repository.CompanyRepository;
+
+@RestController
+@RequestMapping(value = {"/company"})
+public class CompanyRestController {
+
+  @Autowired
+  CompanyRepository companyRepository;
+
+  @GetMapping("/getall")
+  public ResponseEntity<List<Company>> getAll() {
+    List<Company> companyList = new ArrayList<Company>();
+    companyList.addAll(companyRepository.getAllCompanies());
+    return new ResponseEntity<List<Company>>(companyList, HttpStatus.OK);
+  }
+
+}
